e97179b15d9133c52776fdce7069cecffcd00afa
Tim Broughton <TimBroughton@quickenloans.com>
2018-12-07 16:04:12 -0500

    initial commit

---
+++ Assessment7/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment7.Models;
+
+namespace Assessment7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult Dish()
+        {
+            return View();
+        }
+
+        public ActionResult NewDish(Dish newDish)
+        {
+            PartyDBEntities ORM = new PartyDBEntities();
+
+            ORM.Dishes.Add(newDish);
+            ORM.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateGuests(Guest newGuest)
+        {
+
+            PartyDBEntities ORM = new PartyDBEntities();
+
+            ORM.Guests.Add(newGuest);
+            ORM.SaveChanges();
+
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        public ActionResult DishIndex()
+        {
+
+            PartyDBEntities ORM = new PartyDBEntities();
+
+            List<Dish> dishList = ORM.Dishes.ToList();
+
+            ViewBag.DishList = dishList;
+
+            return View();
+        }
+
+
+
+        public ActionResult DeleteDish(int ID)
+        {
+            PartyDBEntities ORM = new PartyDBEntities();
+
+            Dish found = ORM.Dishes.Find(ID);
+
+            ORM.Dishes.Remove(found);
+            ORM.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult EditDish(int ID)
+        {
+            PartyDBEntities ORM = new PartyDBEntities();
+
+            Dish found = ORM.Dishes.Find(ID);
+
+            return View(found);
+
+        }
+
+        public ActionResult ConfirmDishEdit(Dish updatedDish)
+        {
+            PartyDBEntities ORM = new PartyDBEntities();
+
+            Dish oldDish = ORM.Dishes.Find(updatedDish.DishID);
+
+            oldDish.DishName = updatedDish.DishName;
+            oldDish.DishDescription = updatedDish.DishDescription;
+            oldDish.FoodOption = updatedDish.FoodOption;
+            oldDish.PersonName = updatedDish.PersonName;
+            oldDish.PhoneNumber = updatedDish.PhoneNumber;
+
+            ORM.Entry(oldDish).State = System.Data.Entity.EntityState.Modified;
+            ORM.SaveChanges();
+
+            return RedirectToAction("DishIndex");
+        }
+
+
+    }
+}
---
+++ Assessment7/Scripts/JavaScript.js
@@ -0,0 +1,7 @@
+﻿function DeleteChoice(dishID) {
+    if (confirm('Are you sure you want to delete???')) {
+        window.location.href = "/Home/DeleteDish/" + dishID;
+    } else {
+        return false;
+    }
+}
